458283fc59108ea7bb1fccd6c650bc18266fcc09
Allen Shenk <allenjshenk@gmail.com>
2018-02-04 18:06:19 -0500

    tab pages set up for lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  tab1Root: any = 'RandomPage'; 
+  tab2Root: any = 'CategoriesPage'; 
+  tab3Root: any = 'FavoritesPage';
+
 
   constructor(public navCtrl: NavController) {
 
