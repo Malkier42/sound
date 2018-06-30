8af0910ff6c84bd49a579f69769e87b28921456b
Allen Shenk <allenjshenk@gmail.com>
2018-06-30 13:31:29 -0400

    commit all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
+import { DataProvider } from '../providers/data/data';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { MyApp } from './app.component';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -21,6 +24,7 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    DataProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
